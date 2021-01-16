ac33c6a1ef1eb5e79c0ab37aa4aec236554aaf70
rjb888 <robert.j.bronson@gmail.com>
2021-01-15 16:56:28 -0800

    Updated for roomdb use, removed fab, added action bar

--- RoomDBSample/app/src/main/java/com/example/roomdbexample/MainActivity.java
+++ RoomDBSample/app/src/main/java/com/example/roomdbexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.roomdbexample;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
@@ -7,6 +8,8 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -22,10 +25,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Intent intent = new Intent(this, CreateEventActivity.class);
-        FloatingActionButton fab = findViewById(R.id.add_event_btn);
-        fab.setOnClickListener((view) -> startActivity(intent));
+//        Intent intent = new Intent(this, CreateEventActivity.class);
+//        FloatingActionButton fab = findViewById(R.id.add_event_btn);
+//        fab.setOnClickListener((view) -> startActivity(intent));
         eventList = findViewById(R.id.event_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.action_bar, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        Intent intent = new Intent(this, CreateEventActivity.class);
+        startActivity(intent);
+        return true;
     }
 
     @Override
