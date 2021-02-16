8e5b40908b6f59091d124b967f66b975e5e4ae53
rjb888 <robert.j.bronson@gmail.com>
2021-02-16 14:47:12 -0800

    Added spinner menu for conversions

--- app/src/main/java/com/example/unitcoverter/MainActivity.java
+++ app/src/main/java/com/example/unitcoverter/MainActivity.java
@@ -1,11 +1,18 @@
 package com.example.unitcoverter;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button fbtn = findViewById(R.id.convert_btn);
-
+        ArrayAdapter<CharSequence> conversion_adapter =
+                ArrayAdapter.createFromResource(this, R.array.conversions, android.R.layout.simple_spinner_item);
+        Spinner conversion_spinner = findViewById(R.id.conversion_spinner);
+        conversion_adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+        conversion_spinner.setAdapter(conversion_adapter);
         EditText finput = findViewById(R.id.fahrenheit_input);
         TextView ctxt = findViewById(R.id.celcius_text);
         fbtn.setOnClickListener(v -> {
@@ -36,5 +47,19 @@ public class MainActivity extends AppCompatActivity {
             kgText.setText(String.format("%.2f kg", kg));
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.conversion_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        return true;
     }
 }
